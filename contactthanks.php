4b6884d111ce064a766fea62fc25cde64f7a4afa
Nelson Rosales <56582884+nelson2411@users.noreply.github.com>
2021-12-15 22:36:04 +0100

    header and footer changed in all pages

--- contactthanks.php
+++ contactthanks.php
@@ -17,12 +17,12 @@
 	
 	</div>
 	
-	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	<!-- <script src="https://www.google-analytics.com/urchin.js" type="text/javascript">
 	</script>
 	<script type="text/javascript">
 	_uacct = "UA-68528-29";
 	urchinTracker();
-	</script>
+	</script> -->
 
 </body>
 
